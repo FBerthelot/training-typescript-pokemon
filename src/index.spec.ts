8c3d585b275ad3dc2ffe65d8f0193bca5488835b
FBerthelot <florent.berthelot35@gmail.com>
2021-09-15 00:03:40 +0200

    TP4

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,28 +1,120 @@
+import {firstToAttack, fightRound, fight} from './index';
+
+const pikachu = {
+    name: 'pikachu',
+    speed: 50,
+    attack: 20,
+    hp: 100
+};
+
+const salameche = {
+    name: 'salameche',
+    speed: 40,
+    attack: 10,
+    hp: 100
+};
+
+const bulbizarre = {
+    name: 'bulbizarre',
+    speed: 40,
+    attack: 50,
+    hp: 100
+};
+
 describe('pokemon battle', () => {
     describe('when pikachu fight against salamèche', () => {
-        it.todo('should be pikachu to attack first');
-        it.todo('should be pikachu to win');
+        it('should be pikachu to attack first', () => {
+            expect(firstToAttack(pikachu, salameche)).toBe(pikachu);
+        });
+
+        it('should be pikachu to win', () => {
+            expect(fight(salameche, pikachu).name).toBe('pikachu');
+        });
     });
 
     describe('when pikachu fight against bulbizarre', () => {
-        it.todo('should be pikachu to attack first');
-        it.todo('should be bulbizarre to win');
+        it('should be pikachu to attack first', () => {
+            expect(firstToAttack(pikachu, bulbizarre)).toBe(pikachu);
+        });
+        it('should be bulbizarre to win', () => {
+            expect(fight(pikachu, bulbizarre).name).toBe('bulbizarre');
+        });
     });
 
     describe('when attacker pikachu fight against defender pikachunpm', () => {
-        it.todo('should be the attacker pikachu to start when random is around 1');
-        it.todo('should be the defender pikachu to start when random is around 0');
-        it.todo('should be the attacker pikachu to win when random is around 1');
-        it.todo('should be the attacker pikachu to win when random is around 0');
+        const attacker = {
+            name: 'pikachu1',
+            speed: 50,
+            attack: 20,
+            hp: 100
+        };
+
+        const defender = {
+            name: 'pikachu2',
+            speed: 50,
+            attack: 20,
+            hp: 100
+        };
+
+        const MathRandom = Math.random;
+        beforeEach(() => {
+            Math.random = jest.fn();
+        });
+
+        afterEach(() => {
+            Math.random = MathRandom;
+        })
+
+        describe('when random is around 1', () => {
+            beforeEach(() => {
+                (Math.random as jest.Mock).mockImplementation(() => 1);
+            })
+
+            it('should be the attacker pikachu to start', () => {
+                expect(firstToAttack(attacker, defender)).toBe(attacker);
+            });
+
+            it('should be the attacker pikachu to win', () => {
+                expect(fight(attacker, defender).name).toBe('pikachu1');
+            });
+        });
+
+        describe('when random is around 0', () => {
+            beforeEach(() => {
+                (Math.random as jest.Mock).mockImplementation(() => 0);
+            })
+
+            it('should be the defender pikachu to start', () => {
+                expect(firstToAttack(attacker, defender)).toBe(defender);
+            });
+
+            it('should be the defender pikachu to win', () => {
+                expect(fight(attacker, defender).name).toBe('pikachu2');
+            });
+        })
     });
 
-    describe('round', () => {
-        it.todo('shoud make pikachu loose 10 HP when salamèche attack him');
-        it.todo('shoud make pikachu loose 50 HP when bulbizarre attack him');
-        it.todo('shoud make salamèche loose 20 HP when pikachu attack him');
-        it.todo('shoud make salamèche loose 50 HP when bulbizarre attack him');
-        it.todo('shoud make bulbizarre loose 10 HP when salamèche attack him');
-        it.todo('shoud make bulbizarre loose 20 HP when pikachu attack him');
-        it.todo('shoud make pikachu loose 10 HP when pikachu attack him');
+    describe('fightRound', () => {
+        it('shoud make pikachu loose 10 HP when salamèche attack him', () => {
+            expect(fightRound(salameche, pikachu).hp).toBe(90);
+        });
+        it('shoud make pikachu loose 50 HP when bulbizarre attack him', () => {
+            expect(fightRound(bulbizarre, pikachu).hp).toBe(50);
+        });
+        it('shoud make salamèche loose 20 HP when pikachu attack him', () => {
+            expect(fightRound(pikachu, salameche).hp).toBe(80);
+        });
+        it('shoud make salamèche loose 50 HP when bulbizarre attack him', () => {
+            expect(fightRound(bulbizarre, salameche).hp).toBe(50);
+        });
+        it('shoud make bulbizarre loose 10 HP when salamèche attack him', () => {
+            expect(fightRound(salameche, bulbizarre).hp).toBe(90);
+        });
+        it('shoud make bulbizarre loose 20 HP when pikachu attack him', () => {
+            expect(fightRound(pikachu, bulbizarre).hp).toBe(80);
+        });
+        it('shoud make pikachu loose 20 HP when pikachu attack him', () => {
+            expect(fightRound(pikachu, pikachu).hp).toBe(80);
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello world');
+export const firstToAttack = (
+    attacker: {name: string, hp: number, attack: number, speed: number},
+    defender: {name: string, hp: number, attack: number, speed: number}
+    ): {name: string, hp: number, attack: number, speed: number} => {
+        if(attacker.speed === defender.speed) {
+            return Math.random() > 0.5 ? attacker : defender
+        }
+        return attacker.speed > defender.speed ? attacker : defender;
+    };
+
+export const fightRound = (
+    attacker: {name: string, hp: number, attack: number, speed: number},
+    defender: {name: string, hp: number, attack: number, speed: number}
+    ): {name: string, hp: number, attack: number, speed: number} => {
+        return {
+            ...defender,
+            hp: defender.hp - attacker.attack
+        };
+    }
+
+export const fight = (
+    attacker: {name: string, hp: number, attack: number, speed: number},
+    defender: {name: string, hp: number, attack: number, speed: number}
+    ): {name: string, hp: number, attack: number, speed: number} => {
+        let currentAttacker = firstToAttack(attacker, defender);
+        let currentDefender = currentAttacker === attacker ? defender : attacker;
+
+        while(currentAttacker.hp > 0 && currentDefender.hp > 0) {
+            const newAttacker = fightRound(currentAttacker, currentDefender);
+            currentDefender = currentAttacker;
+            currentAttacker = newAttacker;
+        }
+
+        return currentDefender;
+    }
