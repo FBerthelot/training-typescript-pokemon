0849c9986e57b9ada84379395dd02cf98fc77503
FBerthelot <florent.berthelot35@gmail.com>
2021-09-14 21:40:56 +0200

    TP3

---
+++ src/index.spec.ts
@@ -0,0 +1,28 @@
+describe('pokemon battle', () => {
+    describe('when pikachu fight against salamèche', () => {
+        it.todo('should be pikachu to attack first');
+        it.todo('should be pikachu to win');
+    });
+
+    describe('when pikachu fight against bulbizarre', () => {
+        it.todo('should be pikachu to attack first');
+        it.todo('should be bulbizarre to win');
+    });
+
+    describe('when attacker pikachu fight against defender pikachunpm', () => {
+        it.todo('should be the attacker pikachu to start when random is around 1');
+        it.todo('should be the defender pikachu to start when random is around 0');
+        it.todo('should be the attacker pikachu to win when random is around 1');
+        it.todo('should be the attacker pikachu to win when random is around 0');
+    });
+
+    describe('round', () => {
+        it.todo('shoud make pikachu loose 10 HP when salamèche attack him');
+        it.todo('shoud make pikachu loose 50 HP when bulbizarre attack him');
+        it.todo('shoud make salamèche loose 20 HP when pikachu attack him');
+        it.todo('shoud make salamèche loose 50 HP when bulbizarre attack him');
+        it.todo('shoud make bulbizarre loose 10 HP when salamèche attack him');
+        it.todo('shoud make bulbizarre loose 20 HP when pikachu attack him');
+        it.todo('shoud make pikachu loose 10 HP when pikachu attack him');
+    });
+});
